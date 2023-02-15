ec237a240ab6bbeb2c39b353a4d6960e5dc112e9
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-15 14:43:25 +0530

    Welcome to Functional Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FunctionalProgramss
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programming");
+            PowerOfTwo.PowerOfTwoNumber();
+            HarmonicNo.Harmonic();
+            LargestNumber.FindTheLargestNumber();
+            EvenOddNo.CheckingNoEvenOrOdd();
+            VowelAndConsonant.CheckTheCharacter();
+            SwappingNo.SwappingTheNumber();
+
+
+
+
+        }
+    }
+}
