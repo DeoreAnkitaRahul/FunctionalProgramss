7154c38584489f41239ba7c40daa283ff5174ece
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-02-15 14:49:34 +0530

    Print Harmonic Number

---
+++ HarmonicNo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramss
+{
+     class HarmonicNo
+    {
+        public static void Harmonic()
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(" +1/" + i + " ");
+            }
+        }
+
+    }
+
+
+}
